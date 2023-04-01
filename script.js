813e784f063a5d2b5fd75dd67a90fec5bf7be4b2
Princekumar8085 <114429163+Princekumar8085@users.noreply.github.com>
2023-04-01 21:12:18 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,6 @@
 // Your JS code here.
+Your task is to implement "slide in on scroll" i.e. images in main.html need to slide in to their actual positions when scrolling. https://user-images.githubusercontent.com/59690052/164887062-0b2b1806-42b8-4d43-9cea-e464bad326c6.mp4
+
+You need to write code inside the script tag in the main.html file.
+
+Create a function that will add the active classname to the image, when scrolled to it. Pass that function to debounce function.
